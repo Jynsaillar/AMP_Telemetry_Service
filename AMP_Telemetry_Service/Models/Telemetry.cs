4e5d53ae039dd4f4d8f2c79b67071e0dcdb863ef
Jynsaillar <7015218+Jynsaillar@users.noreply.github.com>
2022-09-04 19:37:35 +0200

    Added a controller and a model for telemetry, POST request with parameters now possible (index calculations are not correct, just for testing)

---
+++ AMP_Telemetry_Service/Models/Telemetry.cs
@@ -0,0 +1,9 @@
+namespace AMP_Telemetry_Service.Models
+{
+    public class Telemetry
+    {
+        public bool HIGHEST_BITRATE_POSSIBLE { get; set; }
+        public bool TOO_MANY_BITRATE_SWITCHES { get; set; }
+        public bool TOO_MANY_BUFFERING { get; set; }
+    }
+}
